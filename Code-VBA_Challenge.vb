ac1f88cb60bb2db47299cb6ae306c61f1f0685fd
Meriane Franco <francomeriane@gmail.com>
2021-02-13 19:44:21 -0500

    VBA Challenge Homework

--- Code-VBA_Challenge.vb
+++ Code-VBA_Challenge.vb
@@ -1,11 +1,12 @@
-Sub Summary_Stock() 'Sub  code available at from Extendoffice.com
+
+Sub Summary() 'This is the main Sub to Run
 
 Dim xsh As Worksheet
 Application.ScreenUpdating = False
 For Each xsh In Worksheets
     xsh.Select
     Call stock
-    Call test
+    Call Bonus_Part
 Next
 Application.ScreenUpdating = True
 
@@ -13,74 +14,36 @@ End Sub
 
 
 Sub stock()
+'Variables and fixed information
+    
 
-'* Create a script that will loop through all the stocks for one year and output the following information.
-
-'  * The ticker symbol.
-
-'  * Yearly change from opening price athe beginning of a given year to the closing price at the end of that year.
-
-'  * The percent change from opening price at the beginning of a given year to the closing price at the end of that year.
-
-'  * The total stock volume of the stock.
-
-'* You should also have conditional formatting that will highlight positive change in green and negative change in red.
-
-'* The result should look as follows.
-
-'![moderate_solution](Images/moderate_solution.png)
-
-
-
-' Need 4 buckets for calculation
-'(Ticker symbol[literal string],
-'Yearly change [double],    math: latest close minus first open
-'PChange [double],         math:  Yearly change [double]/first open
-'Volume [integer])          math:  sum of the ticker colume during an year
-
-'Excercise example
-'Find the last ticker
-'Credit card excercise
-
-
-    'Dim ws As Worksheet
+    Cells(1, 9).Value = "Ticker"
+    Cells(1, 10).Value = "Yearly Change"
+    Cells(1, 11).Value = "Percent Change"
+    Cells(1, 12).Value = "Total Stock Volume"
+    Cells(2, 15).Value = "Greatest % Increase"
+    Cells(3, 15).Value = "Percent % Decrease"
+    Cells(4, 15).Value = "Greatest Total Volume"
+    Cells(1, 16).Value = "Ticker"
+    Cells(1, 17).Value = "Value"
+  
+    ' Set an initial variable for holding the last row
     Dim lastrow As Double
     lastrow = 0
-    'Dim i As Integer
-
-'Build the titles
-
-
+    
+    ' Set an initial variable for holding the ticker name
+    Dim Ticker_Name As String
+    
+    ' Set an initial variable for holding the yearly Change
+    Dim YChange As Double
   
+    ' Set an initial variable for holding the % Change
+    Dim PChange As String
   
-  'For Each ws In Worksheets
-  
-Cells(1, 9).Value = "Ticker"
-Cells(1, 10).Value = "Yearly Change"
-Cells(1, 11).Value = "Percent Change"
-Cells(1, 12).Value = "Total Stock Volume"
-Cells(2, 15).Value = "Greatest % Increase"
-Cells(3, 15).Value = "Percent % Decrease"
-Cells(4, 15).Value = "Greatest TOtal Volume"
-Cells(1, 16).Value = "Ticker"
-Cells(1, 17).Value = "Value"
-  ' Find the last row of each worksheet
-
+  ' Find the last row
     lastrow = Cells(Rows.Count, "A").End(xlUp).Row
    
-
-
-  ' Set an initial variable for holding the ticker name
-  Dim Ticker_Name As String
-
-
-  ' Set an initial variable for holding the yearly Change
-  Dim YChange As Double
   
-
-  ' Set an initial variable for holding the % Change
-  Dim PChange As Double
-   
   ' Set an initial variable for holding the total volume
   Dim YVolume_Total As Double
   YVolume_Total = 0
@@ -89,14 +52,15 @@ Cells(1, 17).Value = "Value"
   Dim Ticket_Table_Row As Integer
   Ticket_Table_Row = 2
     
-
-  
-  
+ 
   ' Loop through all tickets
-  For i = 2 To lastrow '*************************************************(need to change the hard code)
+  For i = 2 To lastrow
+ 
+    'Skip the ones that has no value at open
+    If Cells(i, 3).Value <> 0 Then
  
  
-    ' Check if we are still within the same ticker, if it is not...
+     ' Check if we are still within the same ticker, if it is not...
     If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
 
       ' Set the Brand name
@@ -104,7 +68,8 @@ Cells(1, 17).Value = "Value"
       
       'Set The first value of stock at opening
  
-    FirstOpen = Cells(i + 1, 3).Value
+       FirstOpen = Cells(i + 1, 3).Value
+         
       
       ' Add to the Brand Total
      YVolume_Total = YVolume_Total + Cells(i, 7).Value
@@ -121,11 +86,9 @@ Cells(1, 17).Value = "Value"
       ' Reset the Brand Total
       YVolume_Total = 0
 
-    ' If the cell immediately following a row is the same brand...
+    ' If the cell immediately following a row is the same ticket...
     Else
-    
-    
-    
+       
       ' Add to the Volume Total
       YVolume_Total = YVolume_Total + Cells(i, 7).Value
       
@@ -134,13 +97,15 @@ Cells(1, 17).Value = "Value"
      Dim LatestClose As Double
     LatestClose = Cells(i + 1, 6).Value
      
-     
-     If Cells(i, 1).Value = Cells(i + 1, 1).Value Then
+         If Cells(i, 1).Value = Cells(i + 1, 1).Value Then
 
     'fix the firstopen value for the first ticker
-     If FirstOpen = 0 Then
-     FirstOpen = Cells(2, 3).Value
-     End If
+                
+                
+                If FirstOpen = 0 Then
+                FirstOpen = Cells(2, 3).Value
+                End If
+     
      
      
       YChange = LatestClose - FirstOpen
@@ -148,40 +113,31 @@ Cells(1, 17).Value = "Value"
      Range("J" & Ticket_Table_Row).Value = YChange
  
      
-      
-      'PChange [double],         math:  Yearly change [double]/first open
-           PChange = Round(YChange / FirstOpen, 2)
+      'PChange [double]:  Yearly change [double]/first open
+        
+           PChange = FormatPercent(YChange / FirstOpen, 2)
+           
         ' Print YChange in column k
      Range("k" & Ticket_Table_Row).Value = PChange
-      
-      
+         
       End If
     End If
     
- 
+    End If
+    
 Next i
 
 
-'conditional formatting that will highlight positive change in green and negative change in red ***% Change [double]
-
-
-  
-'Cells(2, 3).Interior.ColorIndex = 4
-
 End Sub
 
-Sub test()
-
-
+Sub Bonus_Part()
+'find the last row off the summary table on column I
 lastrow2 = Cells(Rows.Count, "I").End(xlUp).Row
 
-
- 
-
 'Greatest PIncrease
-Dim GPIncrease As Double
+Dim GPIncrease As String
 GPIncrease = Application.WorksheetFunction.Max(Range("k:k"))
-Cells(2, 17).Value = GPIncrease
+Cells(2, 17).Value = FormatPercent(GPIncrease)
 
 For i = 1 To lastrow2
 If Cells(i, 11).Value = GPIncrease Then
@@ -189,14 +145,10 @@ Cells(2, 16).Value = Cells(i, 9).Value
 End If
 Next i
 
-
-
-
-
 'Greatest PDecrease
-Dim GPDecrease As Double
+Dim GPDecrease As String
 GPDecrease = Application.WorksheetFunction.Min(Range("k:k"))
-Cells(3, 17).Value = GPDecrease
+Cells(3, 17).Value = FormatPercent(GPDecrease)
 
 For i = 1 To lastrow2
 If Cells(i, 11).Value = GPDecrease Then
@@ -204,12 +156,10 @@ Cells(3, 16).Value = Cells(i, 9).Value
 End If
 Next i
 
-
 'Greatest Total Volume
 Dim GTotalVolume As Double
 GTotalVolume = Application.WorksheetFunction.Max(Range("L:L"))
 Cells(4, 17).Value = GTotalVolume
-
 
 For i = 1 To lastrow2
 If Cells(i, 12).Value = GTotalVolume Then
@@ -217,18 +167,15 @@ Cells(4, 16).Value = Cells(i, 9).Value
 End If
 Next i
 
-
-
 'formating colors
 For i = 2 To lastrow2
    If (Cells(i, 10).Value >= 0) Then
     Cells(i, 10).Interior.ColorIndex = 4
-     'MsgBox "fica verde"
+     'MsgBox "green"
      Else
      Cells(i, 10).Interior.ColorIndex = 3
-      'MsgBox "fica vermelho"
+      'MsgBox "red"
 End If
 
 Next i
 End Sub
-
